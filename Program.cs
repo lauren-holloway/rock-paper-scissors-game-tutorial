3901cdc8a56c6d3935cbf9960c61cfeca4357d35
Lauren Holloway <hello@laurenholloway.me>
2022-01-10 15:50:14 -0500

    added scorekeeper variables for overall winner

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ public class RockPaperScissorsGameTutorial
 {
     static void Main(string[] args)
     {
+        //Overall scorekeeper variables to track overall score of rounds
+        int playerScore = 0;
+        int cpuScore = 0;
+        
         //Welcome player
         Console.WriteLine("Let's play Rock, Paper, Scissors! We will play 3 times!");
 
@@ -31,31 +35,51 @@ public class RockPaperScissorsGameTutorial
             else if (playerMove == 1 && cpuMove == 2)
             {
                 Console.WriteLine("Paper! You Lose...");
+                cpuScore++;
             }
             else if (playerMove == 1 && cpuMove == 3)
             {
                 Console.WriteLine("Scissors! You win!");
+                playerScore++;
             }
             else if (playerMove == 2 && cpuMove == 1)
             {
                 Console.WriteLine("Rock! You win!");
+                playerScore++;
             }
             else if (playerMove == 2 && cpuMove == 3)
             {
                 Console.WriteLine("Scissors! You lose...");
+                cpuScore++;
             }
             else if (playerMove == 3 && cpuMove == 1)
             {
                 Console.WriteLine("Rock! You lose...");
+                cpuScore++;
             }
             else if (playerMove == 3 && cpuMove == 2)
             {
                 Console.WriteLine("Paper! You win!");
+                playerScore++;
             }
             else
             {
                 Console.WriteLine("Invalid move.");
             }
+        }
+
+        //Add conditional to declare the overall winner of the 3 rounds
+        if(playerScore > cpuScore)
+        {
+            Console.WriteLine("Your Score: {0} | CPU Score: {1}\n YOU'RE THE WINNER!",playerScore,cpuScore);
+        }
+        else if(playerScore < cpuScore)
+        {
+            Console.WriteLine("Your Score: {0} | CPU Score: {1}\n CPU PLAYER IS THE WINNER!", playerScore, cpuScore);
+        }
+        else
+        {
+            Console.WriteLine("Your Score: {0} | CPU Score: {1}\n IT'S A DRAW!", playerScore, cpuScore);
         }
     }
 
