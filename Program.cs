5dafacf9b3feed96ce28af843db9b675fc06dbfa
Lauren Holloway <hello@laurenholloway.me>
2022-01-10 15:21:05 -0500

    added for loop to repeat game 3 times

--- Program.cs
+++ Program.cs
@@ -5,53 +5,59 @@ public class RockPaperScissorsGameTutorial
     static void Main(string[] args)
     {
         //Welcome player
-        Console.WriteLine("Let's play Rock, Paper, Scissors!");
+        Console.WriteLine("Let's play Rock, Paper, Scissors! We will play 3 times!");
 
-        //Player menu
-        Console.WriteLine("Choose your move...");
-        Console.WriteLine("[1] ROCK");
-        Console.WriteLine("[2] PAPER");
-        Console.WriteLine("[3] SCISSORS");
+        //Add a for loop to repeat the game 3x
+        for(int i = 0; i < 3; i++)
+        {
+            //Player menu
+            Console.WriteLine("Choose your move...");
+            Console.WriteLine("[1] ROCK");
+            Console.WriteLine("[2] PAPER");
+            Console.WriteLine("[3] SCISSORS");
 
-        //Get user input for player move
-        int playerMove = Convert.ToInt32(Console.ReadLine());
+            //Get user input for player move
+            int playerMove = Convert.ToInt32(Console.ReadLine());
 
-        //Generate randomized computer player move
-        Random random = new Random();
-        int cpuMove = random.Next(1, 4);
-        
-        //Conditions for choosing a winner
-        if(playerMove == cpuMove)
-        {
-            Console.WriteLine("It's a draw!");
-        }
-        else if(playerMove == 1 && cpuMove == 2)
-        {
-            Console.WriteLine("Paper! You Lose...");
-        }
-        else if(playerMove == 1 && cpuMove == 3)
-        {
-            Console.WriteLine("Scissors! You win!");
-        }
-        else if(playerMove == 2 && cpuMove == 1)
-        {
-            Console.WriteLine("Rock! You win!");
-        }
-        else if(playerMove == 2 && cpuMove == 3)
-        {
-            Console.WriteLine("Scissors! You lose...");
-        }
-        else if(playerMove == 3 && cpuMove == 1)
-        {
-            Console.WriteLine("Rock! You lose...");
-        }
-        else if(playerMove == 3 && cpuMove == 2)
-        {
-            Console.WriteLine("Paper! You win!");
-        }
-        else
-        {
-            Console.WriteLine("Invalid move.");
+            //Generate randomized computer player move
+            Random random = new Random();
+            int cpuMove = random.Next(1, 4);
+
+            //Conditions for choosing a winner
+            if (playerMove == cpuMove)
+            {
+                Console.WriteLine("It's a draw!");
+            }
+            else if (playerMove == 1 && cpuMove == 2)
+            {
+                Console.WriteLine("Paper! You Lose...");
+            }
+            else if (playerMove == 1 && cpuMove == 3)
+            {
+                Console.WriteLine("Scissors! You win!");
+            }
+            else if (playerMove == 2 && cpuMove == 1)
+            {
+                Console.WriteLine("Rock! You win!");
+            }
+            else if (playerMove == 2 && cpuMove == 3)
+            {
+                Console.WriteLine("Scissors! You lose...");
+            }
+            else if (playerMove == 3 && cpuMove == 1)
+            {
+                Console.WriteLine("Rock! You lose...");
+            }
+            else if (playerMove == 3 && cpuMove == 2)
+            {
+                Console.WriteLine("Paper! You win!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid move.");
+            }
         }
     }
+
+        
 }
